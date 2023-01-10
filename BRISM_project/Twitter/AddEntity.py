bbc8563f8f9c63bc7f4b38703de006f425977a2b
Jay <jie.zhang137547@icloud.com>
2023-01-10 22:46:08 +0800

    Add sentiment scores

--- BRISM_project/Twitter/AddEntity.py
+++ BRISM_project/Twitter/AddEntity.py
@@ -9,6 +9,10 @@ import ast
 import time
 import re
 import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import tqdm
+import multiprocessing as mp
+
 ast.literal_eval("{'muffin' : 'lolz', 'foo' : 'kitty'}")
 import nltk
 import ssl
@@ -28,9 +32,10 @@ import spacy
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 merged_data_path = './data/normalized_bri.zip'
+entity_path = '../helper_data/entity_lookup.csv'
 sea_en = ['Thailand', 'SEA','Singapore', 'Malaysia', 'Philippines','Vietnam', 'Myanmar',
           'Brunei','Indonesia','Laos','ASEAN', 'Cambodia']
-entity_path = '../helper_data/entity_lookup.csv'
+
 china_province = ['Heilongjiang', 'Hainan', 'Fujian', 'Henan', 'Shanghai', 'Jiangxi',
                   'Guangdong', 'Shandong', 'Ningxia', 'Shanxi', 'Yunnan', 'Liaoning',
                   'Zhejiang', 'Neimenggu', 'Xinjiang', 'Sichuan', 'Anhui', 'Hebei',
@@ -38,8 +43,16 @@ china_province = ['Heilongjiang', 'Hainan', 'Fujian', 'Henan', 'Shanghai', 'Jian
                   'Tianjin', 'Hunan', 'Xizang', 'Qinghai', 'Chongqing', 'Taiwan', 'Hong Kong',
                   'Macao', 'China']
 # dict_path = 'BRISM_project/helper_data/..json'
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
+def save_zip(df,filename):
+    compression_options = dict(method='zip', archive_name=f'{filename}.csv')
+    df.to_csv(f'{filename}.zip', compression=compression_options)
 class AddEntity(object):
-    def __init__(self, merged_data_path,entity_path,sea_en=None, china_province=None, fortest=False,index= 1051000):
+
+    def __init__(self, merged_data_path,entity_path,sea_en=None, china_province=None, fortest=False,index= 0):
         """
 
         :param merged_data_path: path to the data with tweets and author infos
@@ -48,7 +61,7 @@ class AddEntity(object):
         :param china_province: list
         :param fortest: selecting a small number of dataset for test
         :param index: continuing running the script from defined index to the dataset
-        
+
         return:
               A dataframe with columns 1) tweet_id 2) extracted location entities from look-up table
         """
@@ -62,6 +75,8 @@ class AddEntity(object):
         print('Dataframe columns: ',self.data.columns)
         self.sea_en = sea_en
         self.china_province = china_province
+        if 'lowered_norm_text' in self.data.columns:
+            self.data['lowered_norm_text'] = self.data['lowered_norm_text'].astype('str')
 
     def creat_entity_dict(self, col, ChinaSEA='China',identify_province=True,  save_dict = True, dict_path =None):  # entity df col= entity_en/entity_cn
 
@@ -110,6 +125,9 @@ class AddEntity(object):
                 self.save_dict(entity_dict, dict_path)
             return entity_dict
 
+    # @staticmethod
+
+
     def add_country(self, df, col, colname, china_dict_path=None, sea_dict_path=None,
                     world_dict=None, batch_size= 1000, file_path=None, zip_path=None):  # col = sentences
         # create country column denoting identified places from texts
@@ -136,11 +154,27 @@ class AddEntity(object):
             if world_dict:
                 data_batch[colname] = '' #Worldwide
                 country_dict = world_dict
-            for k, v in country_dict.items():
+            #Assigning locations
+            identified_loc = []
+
+            for j in data_batch[col]:
+                identified = []
+                for k, v in country_dict.items():
+                    locs=country_dict[k].split('|')
+                    locs = [l.lower() for l in locs]
+                    if len(intersection(locs, j.split())) > 0 and any(ext in j for ext in locs):
+                        identified.append(k)
+                if len(identified) != 0:
+                    identified_loc.append(list(set(identified)))
+                else:
+                    identified_loc.append(None)
+
                 # print(k,v)
-                data_batch.loc[(data_batch[col].str.contains(v) == True), [colname]] += ',{}'.format(k)
-            data_batch[colname] = data_batch[colname].map(
-                lambda x: list(set(x.split(',')[1:])) if len(list(set(x.split(',')[1:]))) != 0 else None)
+                # data_batch.loc[(data_batch[col].str.contains(v) == True), [colname]] += ',{}'.format(k)
+                # data_batch[colname] = data_batch[colname].map(
+                #     lambda x: list(set(x.split(',')[1:])) if len(list(set(x.split(',')[1:]))) != 0 else None)
+            assert len(identified_loc) == len(data_batch), 'Lengths are not matched!'
+            data_batch[colname] = identified_loc
             data_batch = data_batch[['id',colname]]
             if file_path:
                 self.save_batch(data_batch,file_path,zip_path)
@@ -151,14 +185,19 @@ class AddEntity(object):
         # return df[['id',colname]]
     def save_batch(self,data_batch,file_path, zip_path=None):
         if zip_path:
-            print('Reading saved zip file and saving to zip file...')
-            existing = pd.read_csv('{}.csv'.format(zip_path), compression='gzip')
-            if data_batch['id'].tolist()[0] not in existing['id'].tolist() and data_batch['id'].tolist()[-1] not in \
-                    existing['id'].tolist():
-                self.append_zip(data_batch,zip_path)
+            NotImplementedError
+            # print('Reading saved zip file and saving to zip file...')
+            # try:
+            #     existing = pd.read_csv('{}.csv'.format(zip_path), compression='gzip')
+            #     if data_batch['id'].tolist()[0] not in existing['id'].tolist() and data_batch['id'].tolist()[-1] not in \
+            #             existing['id'].tolist():
+            #         self.append_zip(data_batch,zip_path)
+            # except:
+            #     print('No available zip file')
+            #     pass
 
         else:
-            print('Reading saved csv file if existed and saving to csv file...')
+
             if os.path.isfile(file_path) == False:
                 data_batch.to_csv(file_path)
             else:
@@ -182,28 +221,75 @@ class AddEntity(object):
         except:
             result = ''
         return result
-    def useLocationTagger(self, df,col,batch_size=1000,file_path=None):
+    def useLocationTagger(self, df,col,batch_size=1000,file_path=None,zip_path=None):
         """See if it can capture additional locations beyond our look-up table."""
+
+        def Remove(tuples):
+            tuples = [t for t in tuples if t]
+            return tuples
         for batch_idx in range(math.ceil(len(df) / batch_size)):
             data_batch = df[['id',col]].iloc[batch_size * batch_idx:batch_size * (batch_idx + 1)]
-            data_batch['locationTagger'] = data_batch[col].map(lambda x: self._tagger(x)).map(lambda x: self._tagger(x))
+            data_batch['locationTagger'] = data_batch[col].map(lambda x: self._tagger(x))
+            data_batch['locationTagger'] = data_batch['locationTagger'].map(lambda x: Remove(x))
+            data_batch['locationTagger'] = data_batch['locationTagger'].map(lambda x: list(set([l for sublist in x for l in sublist])) if len(x) >0 else None)
+            data_batch['locationTagger'] = data_batch['locationTagger'].map(lambda x: list(set([i.capitalize() for i in x])) if x else None)
+            # data_batch['locationTagger'] = data_batch['locationTagger'].map(lambda x: x if len(x)>0 else None)
             data_batch = data_batch[['id','locationTagger']]
             if file_path:
-                self.save_batch(data_batch, file_path)
+                self.save_batch(data_batch, file_path,zip_path)
                 print('Extracted entities for data batch No.{} and saved batch!'.format(batch_idx))
             time.sleep(10)
+    @staticmethod
+    def _extract_org(df,col): #col='origin_text
+        # def get_entity(col):
+        pattern = re.compile(r'([A-Z][a-z]+(?=\s[A-Z])(?:\s[A-Z][a-z]+)+)')
+        captialized_entity = [[j for sub in re.findall(pattern, str(i)) for j in sub] for i in df[col]]
+        captialized_entity = [[x.lower() for x in entity] for entity in captialized_entity]
+        return captialized_entity
+
+    def get_uni_names(self,df,col, file_path=None):
+        captialized_entity = self._extract_org(df,col)
+        assert len(captialized_entity) == len(df), "Lengths are not matched!"
+        uni_names = []
+        keywords = ['university', 'school','college','research']
+        for entity in captialized_entity:
+            if any(ele in entity for ele in keywords):
+                uni_names.append(entity)
+            else:
+                uni_names.append(None)
+            # scholarship_name = [i for i in entity if 'scholarship' in i]
+            # scholarship_names.append(scholarship_name)
+        assert len(uni_names) == len(df), "Lengths are not matched!"
+        df['uni_entities'] = uni_names
+        df['captialized_entities'] = captialized_entity
+        subset = df[['id','uni_entities','captialized_entities']]
+        if file_path:
+            subset.to_csv(file_path)
+        return subset
+    @staticmethod
+    def vader(i):  # adopted from https://github.com/edmangog/The-BRI-on-Twitter/blob/master/3.NLP/3.Sentiment%20Analysis.py
+        analyser = SentimentIntensityAnalyzer()
+        result = analyser.polarity_scores(i).get('compound')
+        return result
+    def assign_sentiment(self,df):
+        pool = mp.Pool()
+        sentiment_list = list(tqdm.tqdm(pool.imap(self.vader, df['origin_text'].fillna('')),
+                                        total=len(df['origin_text'].fillna(''))))
+        # df['sentiment'] = sentiment_list
+        return sentiment_list
+
+
+
     def subset(self, df,col,subsetRule_path = '', filename=None): # col = labelled attribute/column in subsetRule_path
         subsetAttribute = pd.read_csv(subsetRule_path) #[id, locationTaggedProperty]
         subsetAttribute = subsetAttribute.where(pd.notnull(subsetAttribute), None)
         merged_df = df.merge(subsetAttribute, how = 'left', on = 'id')
         merged_df = merged_df[merged_df[col].isnull()==False]
         if filename:
-            self.save_zip(merged_df,filename)
+            save_zip(merged_df,filename)
             print('Saved dataset with labelled attribute!')
-    @staticmethod
-    def save_zip(df,filename):
-        compression_options = dict(method='zip', archive_name=f'{filename}.csv')
-        df.to_csv(f'{filename}.zip', compression=compression_options)
+        return merged_df
+
     @staticmethod
     def append_zip(df, filename):
         df.to_csv('{}.csv'.format(filename), mode='a', compression='gzip')
@@ -219,24 +305,31 @@ class AddEntity(object):
 
 
 
+#
+#
+# if __name__ == '__main__':
+#     add_entity = AddEntity(merged_data_path,entity_path,sea_en=sea_en, china_province=china_province,fortest=False,index= 0)
+#
+#     # china_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='China',identify_province=True,  save_dict = True,dict_path ='../helper_data/china_dict.json')
+#     # sea_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='SEA',identify_province=False,  save_dict = True, dict_path ='../helper_data/sea_dict.json')
+#     # world_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='World', identify_province=False, save_dict=True,
+#     #                                         dict_path='../helper_data/world_dict.json')
+#     print('Tagging China and SEA locations....')
+#     CnSEAsubset = add_entity.add_country(add_entity.data,col='lowered_norm_text',colname='China_SEA',
+#                                          china_dict_path='../helper_data/china_dict.json',sea_dict_path='../helper_data/sea_dict.json',world_dict=None,
+#                                          batch_size= 1000,file_path='./data/China_SEA_tagged.csv',zip_path=None)
+#
+#     print('Tagging locations using locationtagger....')
+#     LocTaggerSubset = add_entity.useLocationTagger(add_entity.data,col = 'lowered_norm_text',
+#                                                    batch_size=1000,file_path='./data/LocTagged.csv')
+#
+#     merged_df = add_entity.subset(add_entity.data, 'China_SEA', subsetRule_path='./data/China_SEA_tagged.csv',filename='bri_sea_cn')
+#     merged_df = add_entity.subset(merged_df, 'LocTag', subsetRule_path='./data/LocTagged.csv',
+#                       filename='bri_sea_cn')
+#     print('Extracting capitalized entities....')
+#     uni_df = add_entity.get_uni_names(merged_df,'lowered_norm_text','./data/capitalized_entities.csv')
 
-
-if __name__ == '__main__':
-    add_entity = AddEntity(merged_data_path,entity_path,sea_en=sea_en, china_province=china_province,fortest=False,index= 1051000)
-
-    # china_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='China',identify_province=True,  save_dict = True,dict_path ='../helper_data/china_dict.json')
-    # sea_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='SEA',identify_province=False,  save_dict = True, dict_path ='../helper_data/sea_dict.json')
-    # world_dict = add_entity.creat_entity_dict('entity_en', ChinaSEA='World', identify_province=False, save_dict=True,
-    #                                         dict_path='../helper_data/world_dict.json')
-    # print('Tagging China and SEA locations....')
-    CnSEAsubset = add_entity.add_country(add_entity.data,col='lowered_norm_text',colname='China_SEA',
-                                         china_dict_path='../helper_data/china_dict.json',sea_dict_path='../helper_data/sea_dict.json',world_dict=None,
-                                         batch_size= 1000,file_path='./data/China_SEA_tagged.csv',zip_path=None)
-    add_entity.subset(add_entity.data, 'China_SEA', subsetRule_path='./data/China_SEA_tagged.csv', filename='bri_sea_cn')
-    # print('Tagging locations using locationtagger....')
-    # LocTaggerSubset = add_entity.useLocationTagger(add_entity.data,col = 'lowered_norm_text',
-    #                                                batch_size=1000,file_path='./data/LocTagged.csv')
-
+#
 
 
 
